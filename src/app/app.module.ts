a3600fd69831fcafa111d64134134cfc3652f5ac
dario zubaray <dzubaray@gmail.com>
2018-11-11 17:43:43 -0300

    creando los componentes basicos de la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,26 @@ import { NgModule } from '@angular/core';
 import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+
+import { APP_ROUTING } from './app.routes';
+import { PeliculaComponent } from './components/pelicula/pelicula.component';
+import { BuscarComponent } from './components/buscar/buscar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PeliculaComponent,
+    BuscarComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    JsonpModule
+    JsonpModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
