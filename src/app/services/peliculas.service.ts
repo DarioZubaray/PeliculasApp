36d0a55715ee20e93b1e726053029ec901500a62
dario zubaray <dzubaray@gmail.com>
2018-11-11 17:09:23 -0300

    cambiando la obtencion de datos en el servicio con el modulo jsonP

--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { Jsonp, Http } from '@angular/http';
+import { Jsonp } from '@angular/http';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -11,12 +11,12 @@ export class PeliculasService {
   private apikey: string = "208e86a842adb3d0297f99095bb0d427";
   private urlMovieDb: string = "https://api.themoviedb.org/3";
 
-  constructor( private jsonp: Jsonp, private http: Http ) { }
+  constructor( private jsonp: Jsonp ) { }
 
   getPopulares() {
-    let url = `${ this.urlMovieDb }/discover/movie?sort_by=popularity.desc&api_key=${ this.apikey }&language=es`;
+    let url = `${ this.urlMovieDb }/discover/movie?sort_by=popularity.desc&api_key=${ this.apikey }&language=es&callback=JSONP_CALLBACK`;
 
-    return this.http.get( url ).pipe(map( res => res.json() ));
+    return this.jsonp.get( url ).pipe(map( res => res.json() ));
   }
 
 }
