596db133f763a7c93126587010ff4de28541545d
dario zubaray <dzubaray@gmail.com>
2018-11-11 21:50:02 -0300

    busqueda del navbar redireccionando a la ruta /buscar/busqueda y tomando los parametros para realizar la misma

--- src/app/components/buscar/buscar.component.ts
+++ src/app/components/buscar/buscar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PeliculasService } from '../../services/peliculas.service';
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-buscar',
@@ -10,7 +11,15 @@ export class BuscarComponent implements OnInit {
 
   buscar: string = "";
 
-  constructor( private _ps: PeliculasService ) { }
+  constructor( private _ps: PeliculasService, private _ar: ActivatedRoute ) {
+    this._ar.params.subscribe( params => {
+      console.log('params[texto]' + params['texto']);
+      if( params['texto'] ) {
+        this.buscar = params['texto'];
+        this.buscarPelicula();
+      }
+    });
+  }
 
   ngOnInit() {
   }
